c9a7918b24ed19298b2d321513ed4a3a05eef968
Linux User <linuxuser@linux-d2006-03.domenis.local>
2024-09-03 16:04:08 +0300

    Completed

---
+++ nodes/publisher.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+rospy.init_node('publisher')
+#rate = rospy.Rate(2)
+pub = rospy.Publisher('/message', String, queue_size=10)
+message = rospy.get_param('~message', 'Hello World!')
+rate_param = rospy.get_param('~rate', 2)
+rate = rospy.Rate(rate_param)
+while not rospy.is_shutdown():
+    pub.publish(message)
+    rate.sleep()
